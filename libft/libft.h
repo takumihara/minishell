91f938d4173b114d097573abbaede908ba231dc5
takumihara <takumihara1226@gmail.com>
2021-09-13 18:28:14 +0900

    update: libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -1,38 +1,74 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <unistd.h>
 # include <stdlib.h>
-# include "../../push_swap/libft/get_next_line.h"
-# include "../../push_swap/libft/list.h"
-# include "../../push_swap/push_swap.h"
 
-int				ft_atoi(char *str);
-size_t			ft_strlen(char const *str);
-void			*ft_memmove(void *dst, const void *src, size_t len);
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+// Part 1 - Libc functions
+
+int				ft_isascii(int c);
+int				ft_isprint(int c);
+int				ft_isalpha(int c);
+int				ft_isdigit(int c);
+int				ft_isalnum(int c);
+
+int				ft_tolower(int c);
+int				ft_toupper(int c);
+size_t			ft_strlen(const char *str);
+size_t			ft_strlcpy(char *dst, const char *src, size_t size);
+size_t			ft_strlcat(char *dst, const char *src, size_t size);
+
+char			*ft_strchr(const char *str, int c);
+char			*ft_strrchr(const char *str, int c);
+char			*ft_strnstr(const char *haystack,
+					const char *needle, size_t len);
 int				ft_strncmp(const char *str1, const char *str2, size_t n);
-int				put_err(char *msg);
-void			*assign_mem(void **dst, void *src);
 
-char			*get_next_line(int fd);
+void			*ft_memset(void *b, int c, size_t len);
+void			ft_bzero(void *s, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memmove(void *dst, const void *src, size_t len);
 
-// ft_lstnew: initialize a new node with its content being 'content'
-t_list			*ft_lstnew(int content);
-// ft_lstadd_front: add a new node represented by 'node'
-// into the last place of list
-// ('lst' doesn't have to be the head node, one of the nodes of the list)
-t_list			*ft_lstadd_back(t_list **lst, t_list *node);
-// ft_lstlast: detect the last node
-t_list			*ft_lstlast(t_list *lst);
-// ft_lstfirst: detect the first node
-t_list			*ft_lstfirst(t_list *lst);
-// ft_lstadd_front: add a new node represented by 'node'
-// into the first place of list
-// ('lst' doesn't have to be the head node, one of the nodes of the list)
-void			ft_lstadd_front(t_list **lst, t_list *node);
-// ft_lstsize: return the number of nodes in the stack
+void			*ft_memchr(const void *s, int c, size_t n);
+int				ft_memcmp(const void *s1, const void *s2, size_t n);
+char			*ft_strdup(const char *str);
+void			*ft_calloc(size_t count, size_t size);
+
+/*
+** Part 2 - Additional functions
+*/
+
+char			*ft_substr(char const *s, unsigned int start, size_t len);
+char			*ft_strjoin(char const *s1, char const *s2);
+char			*ft_strtrim(char const *s1, char const *set);
+char			**ft_split(char const *s, char c);
+char			*ft_itoa(int n);
+char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
+void			ft_putchar_fd(char c, int fd);
+void			ft_putstr_fd(char *s, int fd);
+void			ft_putendl_fd(char *s, int fd);
+void			ft_putnbr_fd(int n, int fd);
+
+// Bonus part
+
+t_list			*ft_lstnew(void *content);
+void			ft_lstadd_front(t_list **lst, t_list *new);
 int				ft_lstsize(t_list *lst);
-void			ft_lstdelone(t_list *lst, void (*del)(int));
-void			ft_lstclear(t_list **lst, void (*del)(int));
+t_list			*ft_lstlast(t_list *lst);
+void			ft_lstadd_back(t_list **lst, t_list *new);
+void			ft_lstdelone(t_list *lst, void (*del)(void *));
+void			ft_lstclear(t_list **lst, void (*del)(void*));
+void			ft_lstiter(t_list *lst, void (*f)(void *));
+t_list			*ft_lstmap(t_list *lst,
+					 void *(*f)(void *), void (*del)(void *));
 
-#endif //LIBFT_H
+// Original
+
+#endif
