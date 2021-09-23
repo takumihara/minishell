0d471993c71cfe92b1927750afe1d77e187c8e60
moromin <34294957+moromin@users.noreply.github.com>
2021-09-23 12:29:48 +0900

    Update: move new_token_string()

--- lexer/lexer_utils.c
+++ lexer/lexer_utils.c
@@ -2,6 +2,26 @@
 #include "lexer.h"
 #include "../libft/libft.h"
 
+t_token	*new_token_string(t_lexer *lexer)
+{
+	t_token	*token;
+	char	*str;
+	size_t	start;
+
+	token = (t_token *)malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	str = &(lexer->input[lexer->position]);
+	printf("str:%s\n", str);
+	start = lexer->position;
+	while (!ft_strchr(DELIMITER, lexer->ch))
+		read_char(lexer);
+	token->type = STRING;
+	token->literal.len = lexer->position - start;
+	token->literal.str = str;
+	return (token);
+}
+
 void	read_char(t_lexer *lexer)
 {
 	if (lexer->read_position == ft_strlen(lexer->input))
@@ -31,25 +51,5 @@ t_token	*new_token(t_token_type token_type, t_lexer *lexer, size_t len)
 	token->type = token_type;
 	token->literal.str = &lexer->input[lexer->position];
 	token->literal.len = len;
-	return (token);
-}
-
-t_token	*new_token_string(t_lexer *lexer)
-{
-	t_token	*token;
-	char	*str;
-	size_t	start;
-
-	token = (t_token *)malloc(sizeof(t_token));
-	if (!token)
-		return (NULL);
-	str = &(lexer->input[lexer->position]);
-	printf("str:%s\n", str);
-	start = lexer->position;
-	while (!ft_strchr(DELIMITER, lexer->ch))
-		read_char(lexer);
-	token->type = STRING;
-	token->literal.len = lexer->position - start;
-	token->literal.str = str;
 	return (token);
 }
