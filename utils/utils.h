fa1f580a6a898ec41180376c91851c41e3ff08a8
moromin <34294957+moromin@users.noreply.github.com>
2021-10-03 20:48:52 +0900

    Add: strstr to wildcard pattern matching

--- utils/utils.h
+++ utils/utils.h
@@ -9,5 +9,6 @@ bool	assign_mem(void **dst, void *src);
 char	*strappend(char *dst, const char *src, size_t l);
 char	*strldup(const char *str, size_t size);
 char	*ft_strcat(char *dest, char *src);
+char	*ft_strstr(char *str, char *to_find);
 
 # endif //UTILS_H
