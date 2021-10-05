4c274e280943942bc241d7cff4f826fa59a665c0
takumihara <takumihara1226@gmail.com>
2021-10-05 17:56:18 +0900

    fix: when home was not set

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,11 +6,18 @@ void	cd(int argc, char **argv)
 {
 	char	*path;
 	if (argc == 1)
+	{
 		path = getenv("HOME");
-	else if (ft_strlen(argv[1]) == 0)
+		if (!path)
+		{
+			ft_putendl_fd("minishell: cd: HOME not set", 2);
+			return ;
+		}
+	}
+	else if (argv[1][0] == '\0')
 		return ;
 	else
 		path = argv[1];
 	if (chdir(path) == -1)
-		perror("chdir");
+		perror("minishell: cd");
 }
