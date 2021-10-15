e925498c669c0ef8ae56a7f60f21c4f19773940b
moromin <34294957+moromin@users.noreply.github.com>
2021-10-15 15:41:22 +0900

    Fix: delete next_var

--- builtin/builtin_unset.c
+++ builtin/builtin_unset.c
@@ -31,27 +31,24 @@ t_env_var	*delete_env_var(char *key, t_env_var *env_vars)
 {
 	t_env_var	*head_var;
 	t_env_var	*pre_var;
-	t_env_var	*next_var;
 
 	head_var = env_vars;
 	pre_var = NULL;
 	while (env_vars)
 	{
-		next_var = env_vars->next;
 		if (!ft_strcmp(key, env_vars->key))
 		{
 			if (pre_var)
-				pre_var->next = next_var;
+				pre_var->next = env_vars->next;
+			else
+				head_var = env_vars->next;
 			free(env_vars->key);
 			free(env_vars->value);
 			free(env_vars);
-			if (!pre_var)
-				return (next_var);
-			else
-				return (head_var);
+			return (head_var);
 		}
 		pre_var = env_vars;
-		env_vars = next_var;
+		env_vars = env_vars->next;
 	}
 	return (head_var);
 }
