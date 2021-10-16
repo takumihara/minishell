0fd52560c6797f4f338609fbeeac8e951564fcc1
moromin <34294957+moromin@users.noreply.github.com>
2021-10-16 19:38:32 +0900

    Remove: get_env_value()

--- expander/expander_env.c
+++ expander/expander_env.c
@@ -10,17 +10,6 @@ size_t	var_strlen(const char *str)
 	return (len);
 }
 
-char	*get_env_value(char *key, t_env_var *env_var)
-{
-	while (env_var)
-	{
-		if (!ft_strcmp(key, env_var->key))
-			return (env_var->value);
-		env_var = env_var->next;
-	}
-	return (NULL);
-}
-
 char	*str_insert(char *data, size_t replace_start, char *env_value, size_t env_value_len)
 {
 	const size_t	origin_len = ft_strlen(data);
