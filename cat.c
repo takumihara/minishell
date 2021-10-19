23c10b45cb003e94ddf1513e61669227e2c31f96
takumihara <takumihara1226@gmail.com>
2021-10-19 10:22:07 +0900

    add: echo test case

---
+++ cat.c
@@ -0,0 +1,10 @@
+//#include <stdio.h>
+//
+//int main(int argc, char **argv, char **envp)
+//{
+//	(void)argc;
+//	(void)argv;
+//	for (int i = 0; envp[i]; i++) {
+//		printf("envp[%d]: %s \n", i, envp[i]);
+//	}
+//}
