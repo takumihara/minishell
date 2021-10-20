cd28c74ed7d8f207b1f6e38e3a8cef69bd67a898
takumihara <takumihara1226@gmail.com>
2021-10-20 11:43:45 +0900

    fix: execve error message bug

--- execute/handle_execute_error.c
+++ execute/handle_execute_error.c
@@ -7,19 +7,29 @@
 
 void	handle_exec_error(char *path, bool is_exec_run)
 {
-	(void)is_exec_run;
-	ft_putstr_fd("minishell: ", STDERR_FILENO);
 	if (!is_exec_run)
 	{
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
 		ft_putstr_fd(path, STDERR_FILENO);
 		ft_putendl_fd(": command not found", STDERR_FILENO);
 		exit(ES_COMMAND_NOT_FOUND);
 	}
 	else
 	{
-		perror(path);
 		if (access(path, X_OK) == -1)
+		{
+			ft_putstr_fd("minishell: ", STDERR_FILENO);
+			ft_putstr_fd(path, STDERR_FILENO);
+			ft_putendl_fd(": Permission denied", STDERR_FILENO);
 			exit(ES_PERMISSION_DENIED);
-		exit(ES_NO_SUCH_FILE);
+		}
+		else if (access(path, F_OK) == -1)
+		{
+			ft_putstr_fd("minishell: ", STDERR_FILENO);
+			ft_putstr_fd(path, STDERR_FILENO);
+			ft_putendl_fd(": No such file or directory", STDERR_FILENO);
+			exit(ES_NO_SUCH_FILE);
+		}
+		exit(EXIT_SUCCESS);
 	}
 }
