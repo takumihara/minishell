3412593be95c167445c33b486a29de9d75276c65
takumihara <takumihara1226@gmail.com>
2021-10-22 20:16:06 +0900

    refac: builtin cd given null arg

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -13,13 +13,11 @@ int	builtin_cd(int argc, char **argv, int no_use, t_env_var **env_vars)
 			ft_putendl_fd("minishell: cd: HOME not set", 2);
 			return (EXIT_FAILURE);
 		}
-		else if (!path[0])
-			return (EXIT_SUCCESS);
 	}
-	else if (argv[1][0] == '\0')
-		return (EXIT_SUCCESS);
 	else
 		path = argv[1];
+	if (path[0] == '\0')
+		return (EXIT_SUCCESS);
 	if (chdir(path) == -1)
 	{
 		ft_putstr_fd("minishell: cd: ", STDERR_FILENO);
