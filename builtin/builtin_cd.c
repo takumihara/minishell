4947600e51f28ded0e649ed94ccacf1ec7961de9
takumihara <takumihara1226@gmail.com>
2021-10-22 19:44:05 +0900

    handle:  unset HOME&& cd && pwd

--- builtin/builtin_cd.c
+++ builtin/builtin_cd.c
@@ -13,6 +13,8 @@ int	builtin_cd(int argc, char **argv, int no_use, t_env_var **env_vars)
 			ft_putendl_fd("minishell: cd: HOME not set", 2);
 			return (EXIT_FAILURE);
 		}
+		else if (!path[0])
+			return (EXIT_SUCCESS);
 	}
 	else if (argv[1][0] == '\0')
 		return (EXIT_SUCCESS);
