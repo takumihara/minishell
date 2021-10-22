972e0935b3e77cab32b8d874d28741b1d95d373a
takumihara <takumihara1226@gmail.com>
2021-10-22 15:46:18 +0900

    update: rm unecessary veriable from execute()

--- execute/execute.c
+++ execute/execute.c
@@ -3,20 +3,18 @@
 #include "internal/execute_internal.h"
 #include "internal/exit_status.h"
 
-int	execute(t_ast_node *node, t_env_var **env_vars)
+int	execute(t_ast_node *root, t_env_var **env_vars)
 {
 	t_executor	*e;
 	int			exit_status;
-	t_ast_node	*root;
 
-	root = node;
-	if (!node)
+	if (!root)
 	{
 		register_env_var_from_literal("?", NULL, ES_SYNTAX_ERROR, env_vars);
 		return (ES_SYNTAX_ERROR);
 	}
 	new_executor(&e, env_vars);
-	exit_status = init_command_line(e, node);
+	exit_status = init_command_line(e, root);
 	delete_ast_nodes(root, NULL);
 	free(e);
 	return (exit_status);
